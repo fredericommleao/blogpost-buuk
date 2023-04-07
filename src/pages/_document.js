729818dfbdaf9b0e4fac8449b49de73de3e00f0c
fredericommleao <fredfredhouse321@hotmail.com>
2023-04-07 18:36:25 -0300

    ajustes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,25 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Document from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="PT-br">
-      <Head />
-      <body>
-      <noscript>
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="PT-br">
+        <Head>
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-562NVWW');`,
+            }}
+          />
+          {/* End Google Tag Manager */}
+        </Head>
+        <body>
+          <noscript>
             <iframe
               src="https://www.googletagmanager.com/ns.html?id=GTM-562NVWW"
               height="0"
@@ -13,9 +27,10 @@ export default function Document() {
               style={{ display: 'none', visibility: 'hidden' }}
             ></iframe>
           </noscript>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
